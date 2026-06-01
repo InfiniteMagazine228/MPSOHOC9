336dc6063da07977929d1af6093fd3521178511a
InfiniteMagazine228 <haduykhanhpham@gmail.com>
2026-06-01 14:48:34 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,15 @@
-from math import math.ceil
+#                    GNU GENERAL PUBLIC LICENSE
+ #                      Version 3, 29 June 2007
+#
+ #Copyright (C) 2026 Free Software Foundation, Inc. <https://fsf.org/>
+ #Everyone is permitted to copy and distribute verbatim copies
+ #of this license document, but changing it is not allowed.
 n=int(input())
-f=math.ceil(n//9 , n % 9 +1)
-print(f)
+k=int(input())
+d=int(input())
+vit= vit * k
+ngan = vit / d
+ga = n - (ngan + vit)
+
+print(ga)
+print(vit)
